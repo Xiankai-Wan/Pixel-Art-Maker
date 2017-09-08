c2d18934ed349bf56a0cb57ea2d60701fa953485
Kevin_Wan <wanxiankai93@gmail.com>
2017-09-08 12:48:15 +0800

    initliaze the js file

--- designs.js
+++ designs.js
@@ -1,16 +1,14 @@
+
+
+
 // Select color input
-var height = $('#input_height').attr('value');
+
 // Select size input
-var width = $('#input_width').attr('value');
+
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
 // Your code goes here!
-console.log("height:" + height);
-console.log("width:" + width);
 
 }
-
-makeGrid();
