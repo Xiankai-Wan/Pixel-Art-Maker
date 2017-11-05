df1112b79ddde419ddee7f1ce275b5813e987c6f
Kevin-Wan <xiankai.wan@gmail.com>
2017-11-05 14:34:41 +0800

    compelet all function

--- designs.js
+++ designs.js
@@ -1,26 +1,24 @@
 'use strict'
-var pA = {
+let pA = {
 	brushColor : "#ff0000",
-  height : 20,
-  width : 20
 };
-
 // Select color input
-var colorPicker = $("#colorPicker");
+let colorPicker = $("#colorPicker");
 colorPicker.on("change", function () {
   pA.brushColor = colorPicker.val();
 });
-// Select size input
-var sizePicker = $("#SubmitSize");
-    sizePicker.on("click",function(){
-      pA.height = $('#input_height').val();
-      pA.width = $('#input_width').val();
-    });
+
+$('#submit').click('change',function(){
+	$('.gird').remove();
+	pA.width = $('#input_width').val();
+	pA.height = $('#input_height').val();
+	layout();
+});
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
 // Your code goes here!
-var gird = $('<div class="gird col-md-1"></div>');
+const gird = $('<div class="gird col-md-1"></div>');
 gird.on('click', function(){
   event.target.style.backgroundColor = pA.brushColor;
 });
@@ -28,11 +26,13 @@ return gird;
 }
 
 //the layout code
-var table = $("#pixel_canvas");
-	for (var row = 0; row < pA.height; row++) {
-		var newRow = $('<div class="row"></div>')
-		for (var col = 0; col < pA.width; col++) {
-			newRow.append(makeGrid());
+function layout(){
+	const table = $("#pixel_canvas");
+		for (let row = 0; row < pA.height; row++) {
+			const newRow = $('<div class="row"></div>')
+			for (let col = 0; col < pA.width; col++) {
+				newRow.append(makeGrid());
+			};
+			table.append(newRow);
 		};
-		table.append(newRow);
-	};
+}
